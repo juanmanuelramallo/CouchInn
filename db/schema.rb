388f966922a0cbf29fba76b3c05d170bc5f6261c
juanmanuelramallo <ramallojuanm@gmail.com>
2015-11-17 23:38:28 -0300

    conflicts resolved

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 20151117185430) do
     t.datetime "updated_at",      null: false
   end
 
+  create_table "reservas", force: :cascade do |t|
+    t.integer  "usuario_id"
+    t.integer  "couch_id"
+    t.integer  "estado"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "reservations", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "couch_id"
@@ -54,6 +62,12 @@ ActiveRecord::Schema.define(version: 20151117185430) do
 
   create_table "tipocs", force: :cascade do |t|
     t.string   "tipo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "typecouches", force: :cascade do |t|
+    t.string   "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -83,5 +97,16 @@ ActiveRecord::Schema.define(version: 20151117185430) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "usuarios", force: :cascade do |t|
+    t.string   "nombre"
+    t.string   "apellido"
+    t.date     "fecnac"
+    t.string   "correo"
+    t.string   "telf"
+    t.integer  "rol"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
