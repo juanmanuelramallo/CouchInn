7ff3a4e4633e2d51a735ae1b844af0900f0a2aca
juanmanuelramallo <ramallojuanm@gmail.com>
2015-11-18 21:52:21 -0300

    Fixing models

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 module ApplicationHelper
   def tipos_array_search
-    tipos_array_search = [['All', 1]]
     aux = Tipoc.all.map { |t| [t.tipo, t.id] }
-
     return aux
   end
 end
--- app/models/searching.rb
+++ app/models/searching.rb
@@ -1,2 +1,3 @@
 class Searching < ActiveRecord::Base
+  belongs_to :user
 end
