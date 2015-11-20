04920dccc59301e5815c3747496e023de6227ef7
Santiago <santiagocobanera@gmail.com>
2015-11-20 00:18:08 -0300

    Agregada opcion ver mis couches

--- app/controllers/couches_controller.rb
+++ app/controllers/couches_controller.rb
@@ -22,8 +22,13 @@ class CouchesController < ApplicationController
     return @couch.user_id == user.id
   end
 
-  def index
+  def main
     @couches = Couch.joins(:user).order('rol desc', 'created_at desc')
+    @fotos = Foto.all
+  end
+
+  def index
+    @couches = Couch.joins(:user).order('rol desc', 'created_at desc').where('user_id = ?', current_user.id)
     @fotos = Foto.all
   end
 
