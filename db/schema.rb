8541a86876babeafb580f542860dd63cfff797a3
ramiro <iglesiasramiro94@hotmail.com>
2015-11-21 20:15:14 -0300

    modificando columna de reservations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120185648) do
+ActiveRecord::Schema.define(version: 20151121230620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,8 @@ ActiveRecord::Schema.define(version: 20151120185648) do
   create_table "reservations", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "couch_id"
-    t.datetime "start_date"
-    t.datetime "end_date"
+    t.date     "start_date"
+    t.date     "end_date"
     t.boolean  "confirmed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
