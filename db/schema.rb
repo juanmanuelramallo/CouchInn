e66657f820b2c0cb92c8f26ddb381572d7269adb
juanmanuelramallo <ramallojuanm@gmail.com>
2015-11-26 19:25:55 -0300

    Questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126193820) do
+ActiveRecord::Schema.define(version: 20151126204025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,15 @@ ActiveRecord::Schema.define(version: 20151126193820) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "description"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.integer  "couch_id"
+    t.integer  "user_id"
+    t.string   "question"
+    t.string   "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reservations", force: :cascade do |t|
