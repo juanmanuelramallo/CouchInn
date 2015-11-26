944a763b70b5ab79d8a328a4a05af71b10b61199
ramiro iglesias <iglesiasramiro94@gmail.com>
2015-11-26 19:08:46 -0300

    acomodando cuadrados

--- db/schema.rb
+++ db/schema.rb
@@ -1,121 +1,3 @@
-<<<<<<< HEAD
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20151126165720) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "couches", force: :cascade do |t|
-    t.integer  "tipo"
-    t.string   "ubicacion"
-    t.integer  "capacidad"
-    t.string   "descripcion"
-    t.integer  "user_id"
-    t.boolean  "reservado"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.boolean  "eliminado"
-  end
-
-  create_table "fotos", force: :cascade do |t|
-    t.integer  "couch_id"
-    t.string   "nombre"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "payments", force: :cascade do |t|
-    t.float    "amount"
-    t.integer  "responseCode"
-    t.string   "responseMessage"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.integer  "user_id"
-  end
-
-  create_table "qualifications", force: :cascade do |t|
-    t.integer  "couch_id"
-    t.integer  "user_id"
-    t.string   "percentage"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "description"
-  end
-
-  create_table "reservations", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "couch_id"
-    t.date     "start_date"
-    t.date     "end_date"
-    t.boolean  "confirmed"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "searchings", force: :cascade do |t|
-    t.integer  "tipo"
-    t.string   "ubicacion_cont"
-    t.integer  "capacidad"
-    t.date     "free_from"
-    t.date     "free_to"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  create_table "tipocs", force: :cascade do |t|
-    t.string   "tipo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "nombre"
-    t.string   "apellido"
-    t.date     "fecnac"
-    t.string   "telf"
-    t.integer  "rol"
-    t.integer  "pais"
-    t.string   "ciudad"
-    t.integer  "genero",                 default: 0
-    t.integer  "failed_attempts",        default: 0,  null: false
-    t.string   "unlock_token"
-    t.datetime "locked_at"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
-
-end
-=======
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -196,7 +78,6 @@ ActiveRecord::Schema.define(version: 20151126193820) do
     t.integer  "capacidad"
     t.date     "free_from"
     t.date     "free_to"
-    t.integer  "user_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
@@ -242,4 +123,3 @@ ActiveRecord::Schema.define(version: 20151126193820) do
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
 end
->>>>>>> 847b471737a2e5ff9922fca723c1c49e73ac7016
