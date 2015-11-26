1366b411b2e5037af298aa61bcfd00090abb1142
ramiro iglesias <iglesiasramiro94@gmail.com>
2015-11-26 19:01:43 -0300

    cuadradito

--- db/schema.rb
+++ db/schema.rb
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 20151126165720) do
     t.integer  "capacidad"
     t.date     "free_from"
     t.date     "free_to"
-    t.integer  "user_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
